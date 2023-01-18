c4ebcd6ffae0e2feae25871170890d30b7eca10a
Raúl <rsotoalbors@gmail.com>
2023-01-18 17:44:31 -0500

    upload setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+import re
+
+
+def get_property(prop, project):
+    result = re.search(r'{}\s*=\s*[\'"]([^\'"]*)[\'"]'.format(prop),
+                       open(f"{project} + '/__init__.py'").read())
+    return result.group(1)
+
+
+project_name = 'lnremote'
+setup(version=get_property('__version__', project_name), )
