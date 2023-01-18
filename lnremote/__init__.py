c04890ea729f1490003f89483cc360928535e0bf
Raúl <rsotoalbors@gmail.com>
2023-01-18 17:44:13 -0500

    add properties to __init__.py

---
+++ lnremote/__init__.py
@@ -0,0 +1,8 @@
+__version__ = '0.7.0'
+__author__ = 'Raul Mojica Soto-Albors'
+__about__ = f"""
+    This software package was developed by {__author__} to be used with
+    Luigs and Neumann GmbH's micromanipulators.
+    \n\nLN-Remote \t Version {__version__}
+    \nCopyright © 2022 by {__author__}
+"""
