92283a6725f3b2e758b10b18309f36071ceb161c
Raúl <rsotoalbors@gmail.com>
2023-04-19 15:43:06 -0400

    checkpoint

---
+++ lnremote/resources_rc.py
@@ -0,0 +1,487 @@
+# Resource object code (Python 3)
+# Created by: object code
+# Created by: The Resource Compiler for Qt version 6.4.2
+# WARNING! All changes made in this file will be lost!
+
+from PySide6 import QtCore
+
+qt_resource_data = b"\
+\x00\x00\x02\xf9\
+<\
+?xml version=\x221.\
+0\x22 encoding=\x22utf\
+-8\x22?>\x0d\x0a<!-- Gene\
+rator: Adobe Ill\
+ustrator 27.1.1,\
+ SVG Export Plug\
+-In . SVG Versio\
+n: 6.00 Build 0)\
+  -->\x0d\x0a<svg vers\
+ion=\x221.1\x22 id=\x22La\
+yer_1\x22 xmlns=\x22ht\
+tp://www.w3.org/\
+2000/svg\x22 xmlns:\
+xlink=\x22http://ww\
+w.w3.org/1999/xl\
+ink\x22 x=\x220px\x22 y=\x22\
+0px\x22\x0d\x0a\x09 viewBox=\
+\x220 0 512 512\x22 st\
+yle=\x22enable-back\
+ground:new 0 0 5\
+12 512;\x22 xml:spa\
+ce=\x22preserve\x22>\x0d\x0a\
+<style type=\x22tex\
+t/css\x22>\x0d\x0a\x09.st0{f\
+ill:#FA0606;}\x0d\x0a<\
+/style>\x0d\x0a<path c\
+lass=\x22st0\x22 d=\x22M2\
+56,0C114.6,0,0,1\
+14.6,0,256s114.6\
+,256,256,256s256\
+-114.6,256-256S3\
+97.4,0,256,0z M2\
+44.7,395.3l-112-\
+112\x0d\x0a\x09c-4.6-4.6-\
+5.9-11.5-3.5-17.\
+4c2.4-5.9,8.3-9.\
+9,14.8-9.9h64v-9\
+6c0-17.7,14.3-32\
+,32-32h32c17.7,0\
+,32,14.3,32,32v9\
+6h64\x0d\x0a\x09c6.5,0,12\
+.3,3.9,14.8,9.9s\
+1.1,12.9-3.5,17.\
+4l-112,112C261.1\
+,401.5,250.9,401\
+.5,244.7,395.3L2\
+44.7,395.3z\x22/>\x0d\x0a\
+</svg>\x0d\x0a\
+\x00\x00\x02\xeb\
+<\
+?xml version=\x221.\
+0\x22 encoding=\x22utf\
+-8\x22?>\x0d\x0a<!-- Gene\
+rator: Adobe Ill\
+ustrator 27.1.1,\
+ SVG Export Plug\
+-In . SVG Versio\
+n: 6.00 Build 0)\
+  -->\x0d\x0a<svg vers\
+ion=\x221.1\x22 id=\x22La\
+yer_1\x22 xmlns=\x22ht\
+tp://www.w3.org/\
+2000/svg\x22 xmlns:\
+xlink=\x22http://ww\
+w.w3.org/1999/xl\
+ink\x22 x=\x220px\x22 y=\x22\
+0px\x22\x0d\x0a\x09 viewBox=\
+\x220 0 512 512\x22 st\
+yle=\x22enable-back\
+ground:new 0 0 5\
+12 512;\x22 xml:spa\
+ce=\x22preserve\x22>\x0d\x0a\
+<style type=\x22tex\
+t/css\x22>\x0d\x0a\x09.st0{f\
+ill:#06A005;}\x0d\x0a<\
+/style>\x0d\x0a<path c\
+lass=\x22st0\x22 d=\x22M2\
+56,512c141.4,0,2\
+56-114.6,256-256\
+S397.4,0,256,0S0\
+,114.6,0,256S114\
+.6,512,256,512z \
+M267.3,116.7l112\
+,112\x0d\x0a\x09c4.6,4.6,\
+5.9,11.5,3.5,17.\
+4s-8.3,9.9-14.8,\
+9.9h-64v96c0,17.\
+7-14.3,32-32,32h\
+-32c-17.7,0-32-1\
+4.3-32-32v-96h-6\
+4\x0d\x0a\x09c-6.5,0-12.3\
+-3.9-14.8-9.9s-1\
+.1-12.9,3.5-17.4\
+l112-112C250.9,1\
+10.5,261.1,110.5\
+,267.3,116.7z\x22/>\
+\x0d\x0a</svg>\x0d\x0a\
+\x00\x00\x02\xf9\
+<\
+?xml version=\x221.\
+0\x22 encoding=\x22utf\
+-8\x22?>\x0d\x0a<!-- Gene\
+rator: Adobe Ill\
+ustrator 27.1.1,\
+ SVG Export Plug\
+-In . SVG Versio\
+n: 6.00 Build 0)\
+  -->\x0d\x0a<svg vers\
+ion=\x221.1\x22 id=\x22La\
+yer_1\x22 xmlns=\x22ht\
+tp://www.w3.org/\
+2000/svg\x22 xmlns:\
+xlink=\x22http://ww\
+w.w3.org/1999/xl\
+ink\x22 x=\x220px\x22 y=\x22\
+0px\x22\x0d\x0a\x09 viewBox=\
+\x220 0 512 512\x22 st\
+yle=\x22enable-back\
+ground:new 0 0 5\
+12 512;\x22 xml:spa\
+ce=\x22preserve\x22>\x0d\x0a\
+<style type=\x22tex\
+t/css\x22>\x0d\x0a\x09.st0{f\
+ill:#06A005;}\x0d\x0a<\
+/style>\x0d\x0a<path c\
+lass=\x22st0\x22 d=\x22M2\
+56,0C114.6,0,0,1\
+14.6,0,256s114.6\
+,256,256,256s256\
+-114.6,256-256S3\
+97.4,0,256,0z M2\
+44.7,395.3l-112-\
+112\x0d\x0a\x09c-4.6-4.6-\
+5.9-11.5-3.5-17.\
+4c2.4-5.9,8.3-9.\
+9,14.8-9.9h64v-9\
+6c0-17.7,14.3-32\
+,32-32h32c17.7,0\
+,32,14.3,32,32v9\
+6h64\x0d\x0a\x09c6.5,0,12\
+.3,3.9,14.8,9.9s\
+1.1,12.9-3.5,17.\
+4l-112,112C261.1\
+,401.5,250.9,401\
+.5,244.7,395.3L2\
+44.7,395.3z\x22/>\x0d\x0a\
+</svg>\x0d\x0a\
+\x00\x00\x02\xe9\
+<\
+?xml version=\x221.\
+0\x22 encoding=\x22utf\
+-8\x22?>\x0d\x0a<!-- Gene\
+rator: Adobe Ill\
+ustrator 27.1.1,\
+ SVG Export Plug\
+-In . SVG Versio\
+n: 6.00 Build 0)\
+  -->\x0d\x0a<svg vers\
+ion=\x221.1\x22 id=\x22La\
+yer_1\x22 xmlns=\x22ht\
+tp://www.w3.org/\
+2000/svg\x22 xmlns:\
+xlink=\x22http://ww\
+w.w3.org/1999/xl\
+ink\x22 x=\x220px\x22 y=\x22\
+0px\x22\x0d\x0a\x09 viewBox=\
+\x220 0 512 512\x22 st\
+yle=\x22enable-back\
+ground:new 0 0 5\
+12 512;\x22 xml:spa\
+ce=\x22preserve\x22>\x0d\x0a\
+<style type=\x22tex\
+t/css\x22>\x0d\x0a\x09.st0{f\
+ill:#FFB91D;}\x0d\x0a<\
+/style>\x0d\x0a<path c\
+lass=\x22st0\x22 d=\x22M0\
+,256c0,141.4,114\
+.6,256,256,256s2\
+56-114.6,256-256\
+S397.4,0,256,0S0\
+,114.6,0,256z M3\
+95.3,267.3l-112,\
+112\x0d\x0a\x09c-4.6,4.6-\
+11.5,5.9-17.4,3.\
+5s-9.9-8.3-9.9-1\
+4.8v-64h-96c-17.\
+7,0-32-14.3-32-3\
+2v-32c0-17.7,14.\
+3-32,32-32h96v-6\
+4\x0d\x0a\x09c0-6.5,3.9-1\
+2.3,9.9-14.8s12.\
+9-1.1,17.4,3.5l1\
+12,112C401.5,250\
+.9,401.5,261.1,3\
+95.3,267.3z\x22/>\x0d\x0a\
+</svg>\x0d\x0a\
+\x00\x00\x06\xc8\
+<\
+?xml version=\x221.\
+0\x22 encoding=\x22utf\
+-8\x22?>\x0d\x0a<!-- Gene\
+rator: Adobe Ill\
+ustrator 27.2.0,\
+ SVG Export Plug\
+-In . SVG Versio\
+n: 6.00 Build 0)\
+  -->\x0d\x0a<svg vers\
+ion=\x221.1\x22 id=\x22La\
+yer_1\x22 xmlns=\x22ht\
+tp://www.w3.org/\
+2000/svg\x22 xmlns:\
+xlink=\x22http://ww\
+w.w3.org/1999/xl\
+ink\x22 x=\x220px\x22 y=\x22\
+0px\x22\x0d\x0a\x09 viewBox=\
+\x220 0 256 256\x22 st\
+yle=\x22enable-back\
+ground:new 0 0 2\
+56 256;\x22 xml:spa\
+ce=\x22preserve\x22>\x0d\x0a\
+<style type=\x22tex\
+t/css\x22>\x0d\x0a\x09.st0{d\
+isplay:none;fill\
+:#FFFFFF;}\x0d\x0a</st\
+yle>\x0d\x0a<circle cl\
+ass=\x22st0\x22 cx=\x2212\
+8.5\x22 cy=\x22127.5\x22 \
+r=\x22126.1\x22/>\x0d\x0a<g>\
+\x0d\x0a\x09<g>\x0d\x0a\x09\x09<path \
+d=\x22M191.3,107.1c\
+-3.9-9.7-7.9-19.\
+3-11.7-29c-8.9-2\
+2.6-17.7-45.3-26\
+.8-67.8c-0.9-2.2\
+-4.1-4.8-6.4-5\x0d\x0a\
+\x09\x09\x09c-8.1-0.8-16.\
+2-0.6-24.4-0.8c-\
+4.8-0.1-6.9,2.2-\
+7.8,6.9c-5.3,28-\
+10.8,55.9-16.4,8\
+3.8c-4.2,20.8-8.\
+6,41.6-13,62.4\x0d\x0a\
+\x09\x09\x09c-0.4,1.7-0.5\
+,3.5-0.8,5.2c-0.\
+8,4.3,0.6,6,5.3,\
+5.6c6.6-0.5,13.2\
+-0.4,19.8-0.4c4.\
+7,0,7.3-2.3,8.2-\
+6.8c6.1-28.7,12.\
+3-57.4,18.5-86.1\
+\x0d\x0a\x09\x09\x09c0.5-2.1,1.\
+3-4.1,2.5-8c5.5,\
+13.1,10.3,24.4,1\
+5.1,35.8c8.3,20,\
+16.7,40,24.8,60.\
+1c1.6,3.9,3.6,5.\
+5,7.8,5.4c7-0.1,\
+14.1,0,21.1,0.3\x0d\
+\x0a\x09\x09\x09c4.3,0.1,6.9\
+-1.1,7.7-5.9c1.7\
+-10.4,4-20.7,5.9\
+-31c2.8-15.3,5.6\
+-30.5,8.3-45.8c1\
+.2-6.7,2.4-13.3,\
+3.6-20c0.3-1.8,0\
+.9-3.6,1.6-5.9\x0d\x0a\
+\x09\x09\x09c32.5,45.7,27\
+.8,118.1-22.4,16\
+2.6c-54,48-129,3\
+4.8-163.3,1.7c12\
+.4,0,23.9,0,35.4\
+,0c1.1,0,2.2,0.2\
+,3.3,0.2\x0d\x0a\x09\x09\x09c25\
+.5,0.4,50.9,0.7,\
+76.4,1.2c5.7,0.1\
+,8.4-1.8,9.2-7.6\
+c1.2-9.1,2.7-18.\
+3,4.8-27.2c1.4-6\
+.1-0.4-7.6-6.3-7\
+.6\x0d\x0a\x09\x09\x09c-30.8,0.\
+2-61.5,0.1-92.3,\
+0.1c-3,0-6,0-9.6\
+,0c2.6-13.8,5-26\
+.7,7.4-39.6c2.7-\
+14,5.5-28,8.2-42\
+c4.3-22.6,8.4-45\
+.2,12.7-67.8\x0d\x0a\x09\x09\
+\x09c1.6-8.6,3.5-17\
+.2,5.1-25.8c0.8-\
+4.4,3.3-6.4,7.6-\
+6.7c31.3-2.8,60.\
+8,1.8,87.4,19.8c\
+12.8,8.6,12.1,9.\
+2,9.1,21\x0d\x0a\x09\x09\x09c-5\
+,19.7-8.6,39.8-1\
+2.8,59.7c-0.3,1.\
+5-0.8,2.9-1.2,4.\
+4C192.3,106.8,19\
+1.8,107,191.3,10\
+7.1z\x22/>\x0d\x0a\x09\x09<path\
+ d=\x22M56.5,24.1c-\
+11.3,57.5-22.4,1\
+13.8-33.7,171C-1\
+3.1,145.1-1.7,63\
+.4,56.5,24.1z\x22/>\
+\x0d\x0a\x09</g>\x0d\x0a</g>\x0d\x0a<\
+/svg>\x0d\x0a\
+\x00\x00\x02\xfc\
+<\
+?xml version=\x221.\
+0\x22 encoding=\x22utf\
+-8\x22?>\x0d\x0a<!-- Gene\
+rator: Adobe Ill\
+ustrator 27.1.1,\
+ SVG Export Plug\
+-In . SVG Versio\
+n: 6.00 Build 0)\
+  -->\x0d\x0a<svg vers\
+ion=\x221.1\x22 id=\x22La\
+yer_1\x22 xmlns=\x22ht\
+tp://www.w3.org/\
+2000/svg\x22 xmlns:\
+xlink=\x22http://ww\
+w.w3.org/1999/xl\
+ink\x22 x=\x220px\x22 y=\x22\
+0px\x22\x0d\x0a\x09 viewBox=\
+\x220 0 512 512\x22 st\
+yle=\x22enable-back\
+ground:new 0 0 5\
+12 512;\x22 xml:spa\
+ce=\x22preserve\x22>\x0d\x0a\
+<style type=\x22tex\
+t/css\x22>\x0d\x0a\x09.st0{f\
+ill:#FFB91D;}\x0d\x0a<\
+/style>\x0d\x0a<path c\
+lass=\x22st0\x22 d=\x22M5\
+12,256C512,114.6\
+,397.4,0,256,0S0\
+,114.6,0,256s114\
+.6,256,256,256S5\
+12,397.4,512,256\
+z M116.7,244.7l1\
+12-112\x0d\x0a\x09c4.6-4.\
+6,11.5-5.9,17.4-\
+3.5c5.9,2.4,9.9,\
+8.3,9.9,14.8v64h\
+96c17.7,0,32,14.\
+3,32,32v32c0,17.\
+7-14.3,32-32,32h\
+-96v64\x0d\x0a\x09c0,6.5-\
+3.9,12.3-9.9,14.\
+8s-12.9,1.1-17.4\
+-3.5l-112-112C11\
+0.5,261.1,110.5,\
+250.9,116.7,244.\
+7L116.7,244.7z\x22/\
+>\x0d\x0a</svg>\x0d\x0a\
+\x00\x00\x02\xeb\
+<\
+?xml version=\x221.\
+0\x22 encoding=\x22utf\
+-8\x22?>\x0d\x0a<!-- Gene\
+rator: Adobe Ill\
+ustrator 27.1.1,\
+ SVG Export Plug\
+-In . SVG Versio\
+n: 6.00 Build 0)\
+  -->\x0d\x0a<svg vers\
+ion=\x221.1\x22 id=\x22La\
+yer_1\x22 xmlns=\x22ht\
+tp://www.w3.org/\
+2000/svg\x22 xmlns:\
+xlink=\x22http://ww\
+w.w3.org/1999/xl\
+ink\x22 x=\x220px\x22 y=\x22\
+0px\x22\x0d\x0a\x09 viewBox=\
+\x220 0 512 512\x22 st\
+yle=\x22enable-back\
+ground:new 0 0 5\
+12 512;\x22 xml:spa\
+ce=\x22preserve\x22>\x0d\x0a\
+<style type=\x22tex\
+t/css\x22>\x0d\x0a\x09.st0{f\
+ill:#FA0606;}\x0d\x0a<\
+/style>\x0d\x0a<path c\
+lass=\x22st0\x22 d=\x22M2\
+56,512c141.4,0,2\
+56-114.6,256-256\
+S397.4,0,256,0S0\
+,114.6,0,256S114\
+.6,512,256,512z \
+M267.3,116.7l112\
+,112\x0d\x0a\x09c4.6,4.6,\
+5.9,11.5,3.5,17.\
+4s-8.3,9.9-14.8,\
+9.9h-64v96c0,17.\
+7-14.3,32-32,32h\
+-32c-17.7,0-32-1\
+4.3-32-32v-96h-6\
+4\x0d\x0a\x09c-6.5,0-12.3\
+-3.9-14.8-9.9s-1\
+.1-12.9,3.5-17.4\
+l112-112C250.9,1\
+10.5,261.1,110.5\
+,267.3,116.7z\x22/>\
+\x0d\x0a</svg>\x0d\x0a\
+"
+
+qt_resource_name = b"\
+\x00\x05\
+\x00o\xa6S\
+\x00i\
+\x00c\x00o\x00n\x00s\
+\x00\x13\
+\x07\xf8\x13\x07\
+\x00c\
+\x00i\x00r\x00c\x00l\x00e\x00-\x00d\x00o\x00w\x00n\x00-\x00r\x00e\x00d\x00.\x00s\
+\x00v\x00g\
+\x00\x13\
+\x0a%\xac\x87\
+\x00c\
+\x00i\x00r\x00c\x00l\x00e\x00-\x00u\x00p\x00-\x00g\x00r\x00e\x00e\x00n\x00.\x00s\
+\x00v\x00g\
+\x00\x15\
+\x05\x08yG\
+\x00c\
+\x00i\x00r\x00c\x00l\x00e\x00-\x00d\x00o\x00w\x00n\x00-\x00g\x00r\x00e\x00e\x00n\
+\x00.\x00s\x00v\x00g\
+\x00\x17\
+\x06\xc5\xc8\xc7\
+\x00c\
+\x00i\x00r\x00c\x00l\x00e\x00-\x00r\x00i\x00g\x00h\x00t\x00-\x00y\x00e\x00l\x00l\
+\x00o\x00w\x00.\x00s\x00v\x00g\
+\x00\x0c\
+\x002%\xa7\
+\x00L\
+\x00N\x00-\x00i\x00c\x00o\x00n\x001\x00.\x00s\x00v\x00g\
+\x00\x15\
+\x06\x17\xa5'\
+\x00c\
+\x00i\x00r\x00c\x00l\x00e\x00-\x00l\x00e\x00f\x00t\x00-\x00y\x00e\x00l\x00l\x00o\
+\x00.\x00s\x00v\x00g\
+\x00\x11\
+\x0d\x16\xfe\xc7\
+\x00c\
+\x00i\x00r\x00c\x00l\x00e\x00-\x00u\x00p\x00-\x00r\x00e\x00d\x00.\x00s\x00v\x00g\
+\
+"
+
+qt_resource_struct = b"\
+\x00\x00\x00\x00\x00\x02\x00\x00\x00\x01\x00\x00\x00\x01\
+\x00\x00\x00\x00\x00\x00\x00\x00\
+\x00\x00\x00\x00\x00\x02\x00\x00\x00\x07\x00\x00\x00\x02\
+\x00\x00\x00\x00\x00\x00\x00\x00\
+\x00\x00\x00\xcc\x00\x00\x00\x00\x00\x01\x00\x00\x0b\xd6\
+\x00\x00\x01\x87G\x91\x8ax\
+\x00\x00\x00h\x00\x00\x00\x00\x00\x01\x00\x00\x05\xec\
+\x00\x00\x01\x85\xeaK\x8c;\
+\x00\x00\x00\xea\x00\x00\x00\x00\x00\x01\x00\x00\x12\xa2\
+\x00\x00\x01\x85\xeaMz~\
+\x00\x00\x00\x98\x00\x00\x00\x00\x00\x01\x00\x00\x08\xe9\
+\x00\x00\x01\x85\xeaMjN\
+\x00\x00\x00\x10\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\
+\x00\x00\x01\x85\xecK\x84\x1c\
+\x00\x00\x00<\x00\x00\x00\x00\x00\x01\x00\x00\x02\xfd\
+\x00\x00\x01\x85\xecJ\x8f\x0a\
+\x00\x00\x01\x1a\x00\x00\x00\x00\x00\x01\x00\x00\x15\xa2\
+\x00\x00\x01\x85\xecK6?\
+"
+
+def qInitResources():
+    QtCore.qRegisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
+
+def qCleanupResources():
+    QtCore.qUnregisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
+
+qInitResources()
