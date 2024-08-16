708db44c4bb230c33bf08ab47384dc7b8916f99c
Raúl <rsotoalbors@gmail.com>
2024-08-16 09:32:38 -0400

    version update

--- lnremote/__init__.py
+++ lnremote/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.8.0'
+__version__ = '0.8.1'
 __author__ = 'Raul Mojica Soto-Albors'
 __about__ = f"""This software package was developed by {__author__} to be used with Luigs and Neumann GmbH's micromanipulators.
     \n\nLN-Remote \t Version {__version__}
