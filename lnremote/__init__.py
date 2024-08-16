15721c47cb731ef42d112432ff66bfe7a465314c
Raúl <rsotoalbors@gmail.com>
2024-08-16 10:50:35 -0400

    linting

--- lnremote/__init__.py
+++ lnremote/__init__.py
@@ -1,6 +1,7 @@
 __version__ = '0.8.1'
 __author__ = 'Raul Mojica Soto-Albors'
-__about__ = f"""This software package was developed by {__author__} to be used with Luigs and Neumann GmbH's micromanipulators.
+__about__ = f"""This software package was developed by {__author__} to be used
+    with Luigs and Neumann GmbH's micromanipulators.
     \n\nLN-Remote \t Version {__version__}
     \nCopyright © 2022 by {__author__}
 """
